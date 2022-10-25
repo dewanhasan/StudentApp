a7bddbbf6d339e0c79ea26bfeb2e9a000360bea0
dewanhasan99 <g00410156@atu.ie>
2022-10-25 16:29:29 +0100

    Promt User for student details

--- src/main/java/ie/atu/week5/StudentApp.java
+++ src/main/java/ie/atu/week5/StudentApp.java
@@ -1,4 +1,24 @@
 package ie.atu.week5;
 
+import java.util.Scanner;
+
 public class StudentApp {
+
+    public static void main(String[] args) {
+        System.out.println("Please enter Student Name: ");
+        Scanner input = new Scanner(System.in);
+        String n1 = input.nextLine();
+        System.out.println("Name: " + n1);
+
+        System.out.println("Please enter Student Email: ");
+        String e1 = input.nextLine();
+        System.out.println("Email: " + e1);
+
+        String c1 = input.nextLine();
+        System.out.println("Please enter Student Course: ");
+        System.out.println("course: " + c1);
+
+
+    }
+
 }
