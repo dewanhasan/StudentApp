3dbf5b70324db34a3db603015ee78534f46bf32b
dewanhasan99 <g00410156@atu.ie>
2022-10-25 17:06:00 +0100

    Added new file new methods

--- src/main/java/ie/atu/week5/StudentApp.java
+++ src/main/java/ie/atu/week5/StudentApp.java
@@ -5,18 +5,44 @@ import java.util.Scanner;
 public class StudentApp {
 
     public static void main(String[] args) {
-        System.out.println("Please enter Student Name: ");
         Scanner input = new Scanner(System.in);
+        System.out.println("Please enter Student Name: ");
         String name1 = input.nextLine();
-        System.out.println("Name: " + name1);
+        StudentClass stuClass = new StudentClass();
+        stuClass.setName(name1);
+        System.out.println("Name: " + stuClass.getName());
+
 
         System.out.println("Please enter Student Email: ");
         String email1 = input.nextLine();
-        System.out.println("Email: " + email1);
+        stuClass.setEmail(email1);
+        System.out.println("Email: " + stuClass.getEmail());
 
-        String course1 = input.nextLine();
+
         System.out.println("Please enter Student Course: ");
-        System.out.println("course: " + course1);
+        String course1 = input.nextLine();
+        stuClass.setCourse(course1);
+        System.out.println("course: " + stuClass.getCourse());
+
+
+        StudentClass stuClass2 = new StudentClass();
+        System.out.println("Please enter Second Student Name: ");
+        String name2 = input.nextLine();
+        stuClass2.setName(name2);
+        System.out.println("Name: " + stuClass2.getName());
+
+
+        System.out.println("Please enter Second Student Email: ");
+        String email2 = input.nextLine();
+        stuClass2.setEmail(email2);
+        System.out.println("Email: " + stuClass2.getEmail());
+
+
+        System.out.println("Please enter Second Student Course: ");
+        String course2 = input.nextLine();
+        stuClass2.setCourse(course2);
+        System.out.println("course: " + stuClass2.getCourse());
+
 
 
     }
